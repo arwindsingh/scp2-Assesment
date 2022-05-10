b7ed00eec231a44a454b1554dd7119b08a10422f
arwindsingh <71174358+arwindsingh@users.noreply.github.com>
2022-05-11 09:27:35 +1200

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Footer extends Component
+{
+    render()
+    {
+        return(
+            <footer className="text-muted py-5">
+  <div className="container">
+    <p className="float-end mb-1">
+     
+    </p>
+    <p className="mb-1"> SCP assesment 02 &copy; 30036198_Anjalika_Patabeige</p>
+  </div>
+</footer>
+        );
+    }
+}
+
+export default Footer;
